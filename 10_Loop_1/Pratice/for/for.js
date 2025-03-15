f889129635e794acd7b7438d5b73849a4cc00e1a
MarcoHoang <103943607+MarcoHoang@users.noreply.github.com>
2025-03-16 00:21:24 +0700

    Add files via upload

---
+++ 10_Loop_1/Pratice/for/for.js
@@ -0,0 +1,15 @@
+// Exercise 1
+
+for (N = 0; N < 5; N++) {
+    console.log('The Number is '+ N);
+};
+
+// Exercise 2
+
+let number = prompt('Nhập vào 1 số dương');
+let sum = 0;
+
+for ( i = 1; i <= number; i++ ) {
+    sum +=i;
+};
+console.log(sum);
